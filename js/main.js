97a5fb600298e7fecfed1d02750ef585d314f558
Mateusz Poplewski <matheush84@gmail.com>
2020-03-14 01:30:46 +0000

    added pictures and background colors

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
+
+/*Galleria initializing*/
+$('.photoset-grid-basic').photosetGrid();
+
+$('.photoset-grid-custom').photosetGrid({
+  // Set the gutter between columns and rows
+  gutter: '5px',
+  // Manually set the grid layout
+  layout: '21',
+  // Wrap the images in links
+  highresLinks: true,
+  // Asign a common rel attribute
+  rel: 'print-gallery',
+
+  onInit: function(){},
+  onComplete: function(){
+    // Show the grid after it renders
+    $('.photoset-grid-custom').attr('style', '');
+  }
+});
+
+
+
+
+
 /*current time function//
 var today = new_Date();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
