7674b121e7e05d5cf92998c3da53457013cb4030
Mateusz Poplewski <matheush84@gmail.com>
2020-03-14 07:56:48 +0000

    fixed pictures sizing

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 
 /*Galleria initializing*/
-$('.photoset-grid-basic').photosetGrid();
 
 $('.photoset-grid-custom').photosetGrid({
   // Set the gutter between columns and rows
   gutter: '5px',
   // Manually set the grid layout
   layout: '21',
-  // Wrap the images in links
+  /*/ Wrap the images in links
   highresLinks: true,
   // Asign a common rel attribute
   rel: 'print-gallery',
@@ -18,7 +17,7 @@ $('.photoset-grid-custom').photosetGrid({
     $('.photoset-grid-custom').attr('style', '');
   }
 });
-
+*/
 
 
 
@@ -41,3 +40,17 @@ document.getElementById('ct').innerHTML = x;
 display_c();
  }
  */
+
+
+ // Create a request variable and assign a new XMLHttpRequest object to it.
+var request = new XMLHttpRequest()
+
+// Open a new connection, using the GET request on the URL endpoint
+request.open("GET", "http://api.weatherapi.com/v1/current.json?key=cca9a8d67994441b8ed161809201303&q=Liverpool", true)
+
+request.onload = function() {
+  // Begin accessing JSON data here
+}
+
+// Send request
+request.send()
