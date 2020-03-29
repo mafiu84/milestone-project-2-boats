acf8801a13d241af33d8eddd2656ecc7bac21c01
Mateusz Poplewski <matheush84@gmail.com>
2020-03-29 15:03:14 +0000

    another attempt to set emailjs template

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,11 +1,23 @@
-var templateParams = {
+/**var templateParams = {
     name: "James",
     notes: "Check this out!"
-};
+};*/
+var template_params = {
+   "from_email": "from_email_value",
+   "name": "name_value",
+   "company": "company_value",
+   "emailaddress": "emailaddress_value",
+   "projectsummary": "projectsummary_value",
+   "date": "date_value",
+   "transport_request": "transport_request_value",
+   "from_name": "from_name_value"
+}
 
+var service_id = "gmail";
+var template_id = "matt";
 
 function sendMail(contactForm) {
-    emailjs.send("gmail", "matt", templateParams, "user_LYCAaBmQkGbAnlvCqR3Dy", {
+    emailjs.send(service_id, template_id, templateParams, "user_LYCAaBmQkGbAnlvCqR3Dy", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "transport_request": contactForm.projectsummary.value
