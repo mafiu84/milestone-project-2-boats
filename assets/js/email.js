0fdfccba1473e9571f2ef85acfa4f30744376588
Mateusz Poplewski <matheush84@gmail.com>
2020-03-29 18:39:18 +0000

    email js set and working along with email template

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,26 +1,10 @@
-/**var templateParams = {
-    name: "James",
-    notes: "Check this out!"
-};*/
-var template_params = {
-   "from_email": "from_email_value",
-   "name": "name_value",
-   "company": "company_value",
-   "emailaddress": "emailaddress_value",
-   "projectsummary": "projectsummary_value",
-   "date": "date_value",
-   "transport_request": "transport_request_value",
-   "from_name": "from_name_value"
-}
-
-var service_id = "gmail";
-var template_id = "matt";
-
 function sendMail(contactForm) {
-    emailjs.send(service_id, template_id, templateParams, "user_LYCAaBmQkGbAnlvCqR3Dy", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "transport_request": contactForm.projectsummary.value
+    emailjs.send('gmail', 'matt', {
+        'from_name': contactForm.name.value,
+        'from_email': contactForm.emailaddress.value,
+        'request': contactForm.projectsummary.value,
+        'from_company': contactForm.company.value,
+        'from_date': contactForm.date.value
     })
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
