a8ea6aafc7be8ac752d238db78650d093a897f8f
Mateusz Poplewski <matheush84@gmail.com>
2020-03-29 14:29:02 +0000

    emailjs now sending emails but template doesnt work yet properly

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,15 +1,19 @@
-function sendMail(contactForm){
-    emailjs.send("gmail", "matt",{
-        "from_name":contactFrom.name.value,
-        "from_email":contactForm.emailaddress.value,
-        "transport_request":contactForm.projectsummary.value
+var templateParams = {
+    name: "James",
+    notes: "Check this out!"
+};
+
+
+function sendMail(contactForm) {
+    emailjs.send("gmail", "matt", templateParams, "user_LYCAaBmQkGbAnlvCqR3Dy", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "transport_request": contactForm.projectsummary.value
     })
-    .then(
-        function(response) {
-            console.log("succes", response);
-        },
-        function(error){
-            console.log("failed", error);
+        .then(function (response) {
+            console.log('SUCCESS!', response.status, response.text);
+        }, function (error) {
+            console.log('FAILED...', error);
         });
-        return false;
+    return false;
 }
